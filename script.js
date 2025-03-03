a7e60f02b91769d62fd087237d3ebfaa17ffa331
Yashaswini Jain <122783078+yoshjayn@users.noreply.github.com>
2025-03-03 18:10:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Complete the js code
-function Car(make, model) {}
+class Car {
+	constructor(make, model){
+		this.make = make;
+		this.model = model;
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+class SportsCar extends Car{
+	constructor(topSpeed){
+		super(make, model)
+		this.topSpeed = topSpeed
+	}
+
+    getTopSpeed(){
+        return this.topSpeed
+    }
+}
 
 // Do not change the code below
 window.Car = Car;
