1869507b7d79a77768a83c72b6db53589641a13a
Yashaswini Jain <122783078+yoshjayn@users.noreply.github.com>
2025-03-03 19:30:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ class Car {
 		this.make = make;
 		this.model = model;
 	}
+    getMakeModel(){
+        return  `${this.make} ${this.model}`
+    }
 }
 
 class SportsCar extends Car{
-	constructor(topSpeed){
+	constructor(make, model,topSpeed){
 		super(make, model)
 		this.topSpeed = topSpeed
 	}
